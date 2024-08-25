dc1d961569737c40f1b6a70559e93dc1435b9097
duythanh7903 <beedyto@gmail.com>
2024-08-25 17:58:01 +0700

    add const auth and user references

--- app/src/main/java/com/social/media/blog/ltd/commons/AppConst.kt
+++ app/src/main/java/com/social/media/blog/ltd/commons/AppConst.kt
@@ -1,10 +1,15 @@
 package com.social.media.blog.ltd.commons
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
 object AppConst {
 
     // NUMBER
     internal const val SPLASH_DELAY_TIME_MS_TYPE = 4000L
 
-    // STRING
-
+    // FIREBASE VARIABLES
+    private val database = FirebaseDatabase.getInstance()
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    val userRef = database.getReference("Users")
 }
