236b4c5685344bb738453c3f2d225892988a587f
duythanh7903 <beedyto@gmail.com>
2024-08-25 13:28:14 +0700

    make ui login screen

--- app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginActivity.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.social.media.blog.ltd.ui.screen.login
 
+import androidx.activity.viewModels
+import com.social.media.blog.ltd.commons.Routes.startRegisterActivity
+import com.social.media.blog.ltd.commons.extention.click
 import com.social.media.blog.ltd.databinding.ActivityLoginBinding
 import com.social.media.blog.ltd.ui.base.BaseActivity
 
 class LoginActivity: BaseActivity<ActivityLoginBinding>() {
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun inflateViewBinding(): ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
 
@@ -13,5 +18,11 @@ class LoginActivity: BaseActivity<ActivityLoginBinding>() {
 
     override fun initView() = binding.apply {
 
+    }
+
+    override fun clickViews() = binding.apply {
+        textRegisterAccount.click {
+            startRegisterActivity(this@LoginActivity)
+        }
     }
 }
---
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.social.media.blog.ltd.ui.screen.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    
+
+}
