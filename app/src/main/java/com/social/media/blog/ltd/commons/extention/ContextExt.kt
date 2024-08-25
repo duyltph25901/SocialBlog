2269cdaad064cd35a1dd58678f289c997aa3e06b
duythanh7903 <beedyto@gmail.com>
2024-08-25 17:55:21 +0700

    create ext

--- app/src/main/java/com/social/media/blog/ltd/commons/extention/ContextExt.kt
+++ app/src/main/java/com/social/media/blog/ltd/commons/extention/ContextExt.kt
@@ -1,7 +1,11 @@
 package com.social.media.blog.ltd.commons.extention
 
 import android.content.Context
+import android.widget.Toast
 import java.util.Locale
+import java.util.regex.Pattern
+
+var toast: Toast? = null
 
 fun Context.setAppLocale(language: String): Context {
     val locale = Locale(language)
@@ -11,3 +15,25 @@ fun Context.setAppLocale(language: String): Context {
     config.setLayoutDirection(locale)
     return createConfigurationContext(config)
 }
+
+fun Context.toastMessage(message: String) {
+    toast?.cancel()
+    toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+    toast!!.show()
+}
+
+fun Context.toastMessageRes(message: Int) {
+    toast?.cancel()
+    toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+    toast!!.show()
+}
+
+fun Context.validateEmail(email: String): Boolean {
+    val emailPattern =
+        "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+    val pattern = Pattern.compile(emailPattern)
+
+    return pattern.matcher(email).matches()
+}
+
+fun Context.validatePassword(password: String): Boolean = password.length >= 6
