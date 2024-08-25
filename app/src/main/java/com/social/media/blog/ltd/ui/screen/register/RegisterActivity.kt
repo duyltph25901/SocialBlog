05577a6f050cb4b81a2cc099db86c6383786085d
duythanh7903 <beedyto@gmail.com>
2024-08-25 17:58:37 +0700

    create logic fun register account

--- app/src/main/java/com/social/media/blog/ltd/ui/screen/register/RegisterActivity.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/register/RegisterActivity.kt
@@ -1,14 +1,28 @@
 package com.social.media.blog.ltd.ui.screen.register
 
+import androidx.activity.viewModels
+import com.social.media.blog.ltd.R
 import com.social.media.blog.ltd.commons.extention.click
+import com.social.media.blog.ltd.commons.extention.toastMessage
+import com.social.media.blog.ltd.commons.extention.toastMessageRes
+import com.social.media.blog.ltd.commons.extention.validateEmail
+import com.social.media.blog.ltd.commons.extention.validatePassword
 import com.social.media.blog.ltd.databinding.ActivityRegisterBinding
 import com.social.media.blog.ltd.ui.base.BaseActivity
+import com.social.media.blog.ltd.ui.dialog.LoadingResponseDialog
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
 
+    private val viewModel: RegisterViewModel by viewModels()
+
+    private lateinit var loadingDialog: LoadingResponseDialog
 
     override fun inflateViewBinding(): ActivityRegisterBinding =
         ActivityRegisterBinding.inflate(layoutInflater)
+
+    override fun initVariable() {
+        loadingDialog = LoadingResponseDialog(this, true)
+    }
 
     override fun initView() = binding.apply {
 
@@ -16,5 +30,46 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
 
     override fun clickViews() = binding.apply {
         icBack.click { finish() }
+        buttonRegister.click { eventRegister() }
+    }
+
+    override fun observerDataSource() = viewModel.apply {
+        isLoadingNetwork.observe(this@RegisterActivity) { isLoading ->
+            if (isLoading) loadingDialog.show() else loadingDialog.cancel()
+        }
+
+        isInsertSuccess.observe(this@RegisterActivity) { isSuccess ->
+            if (isSuccess) {
+                binding.inputUserName.text.clear()
+                binding.inputPassword.text.clear()
+                binding.inputEmailAddress.text.clear()
+                toastMessage("Thanh cong!")
+            }
+        }
+    }
+
+    private fun eventRegister() {
+        val email = binding.inputEmailAddress.text.toString().trim()
+        val userName = binding.inputUserName.text.toString().trim()
+        val password = binding.inputPassword.text.toString().trim()
+        val isInputNull = email.isEmpty() || userName.isEmpty() || password.isEmpty()
+        val isEmail = validateEmail(email)
+        val isPassword = validatePassword(password)
+
+        if (isInputNull) {
+            toastMessageRes(R.string.this_field_cannot_be_left_blank)
+            return
+        }; if (!isEmail) {
+            toastMessageRes(R.string.invalid_email)
+            return
+        }; if (!isPassword) {
+            toastMessageRes(R.string.password_minimum_six_characters)
+            return
+        }
+
+        val jobRegisterAccount = viewModel.register(email, password, userName)
+        loadingDialog.cancelProgress = {
+            jobRegisterAccount.cancel()
+        }
     }
 }
--- app/src/main/java/com/social/media/blog/ltd/ui/screen/register/RegisterViewModel.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/register/RegisterViewModel.kt
@@ -1,6 +1,83 @@
 package com.social.media.blog.ltd.ui.screen.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseUser
+import com.social.media.blog.ltd.commons.AppConst.auth
+import com.social.media.blog.ltd.commons.AppConst.userRef
+import com.social.media.blog.ltd.model.dto.UserModelDTO
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-class RegisterViewModel: ViewModel() {
+class RegisterViewModel : ViewModel() {
+
+    private val _isLoadingNetWork = MutableLiveData<Boolean>()
+    private val _isInsertSuccess = MutableLiveData<Boolean>()
+
+    val isLoadingNetwork: LiveData<Boolean> = _isLoadingNetWork
+    val isInsertSuccess: LiveData<Boolean> = _isInsertSuccess
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, _ ->
+
+    }
+
+    fun register(email: String, password: String, userName: String) =
+        viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
+            _isLoadingNetWork.postValue(true)
+            val userAfterRegister = registerAuthentication(email, password)
+            userAfterRegister?.let { userFirebaseAuth ->
+                val userDto =
+                    getUserDtoFromUserFirebaseAuthentication(userFirebaseAuth, userName, password)
+                _isInsertSuccess.postValue(registerAccountRealTimeDb(userDto))
+            }
+            _isLoadingNetWork.postValue(false)
+
+            throw Exception()
+        }
+
+    private suspend fun registerAuthentication(
+        email: String,
+        password: String
+    ): FirebaseUser? =
+        suspendCoroutine { continuation ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        auth.currentUser?.let {
+                            continuation.resume(it)
+                        }
+                    } else continuation.resume(null)
+                }
+        }
+
+    /**
+     * This fun will be returned results after insert done
+     * If true -> insert successful else failure
+     * **/
+    private suspend fun registerAccountRealTimeDb(userDto: UserModelDTO) =
+        suspendCoroutine { continuation ->
+            userRef.child(userDto.idUser).setValue(userDto)
+                .addOnSuccessListener {
+                    continuation.resume(true)
+                }.addOnFailureListener {
+                    continuation.resume(false)
+                }
+        }
+
+    private fun getUserDtoFromUserFirebaseAuthentication(
+        userFirebase: FirebaseUser,
+        userName: String,
+        password: String
+    ): UserModelDTO =
+        UserModelDTO(
+            idUser = userFirebase.uid,
+            userName = userName,
+            email = userFirebase.email ?: "",
+            password = password,
+        )
 }
