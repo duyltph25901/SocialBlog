99df328748e7a06c086186c6ee45d16c05090c66
duythanh7903 <beedyto@gmail.com>
2024-08-26 22:36:46 +0700

    create logic login

--- app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginActivity.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginActivity.kt
@@ -1,28 +1,126 @@
 package com.social.media.blog.ltd.ui.screen.login
 
+import android.util.Log
 import androidx.activity.viewModels
+import com.social.media.blog.ltd.R
+import com.social.media.blog.ltd.commons.Routes.startHomeActivity
 import com.social.media.blog.ltd.commons.Routes.startRegisterActivity
+import com.social.media.blog.ltd.commons.SharedUtils
 import com.social.media.blog.ltd.commons.extention.click
+import com.social.media.blog.ltd.commons.extention.convertObjectToJson
+import com.social.media.blog.ltd.commons.extention.toastMessageRes
 import com.social.media.blog.ltd.databinding.ActivityLoginBinding
+import com.social.media.blog.ltd.model.dto.UserModelDTO
 import com.social.media.blog.ltd.ui.base.BaseActivity
+import com.social.media.blog.ltd.ui.dialog.LoadingResponseDialog
 
-class LoginActivity: BaseActivity<ActivityLoginBinding>() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>() {
 
     private val viewModel: LoginViewModel by viewModels()
 
-    override fun inflateViewBinding(): ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+    private lateinit var dialogLoading: LoadingResponseDialog
+    private lateinit var listUsers: MutableList<UserModelDTO>
+
+    override fun inflateViewBinding(): ActivityLoginBinding =
+        ActivityLoginBinding.inflate(layoutInflater)
 
     override fun initVariable() {
+        dialogLoading = LoadingResponseDialog(this@LoginActivity, false)
 
-    }
-
-    override fun initView() = binding.apply {
-
+        listUsers = mutableListOf()
     }
 
     override fun clickViews() = binding.apply {
-        textRegisterAccount.click {
-            startRegisterActivity(this@LoginActivity)
+        textRegisterAccount.click { startRegisterActivity(this@LoginActivity) }
+        icGg.click { toastMessageInComing() }
+        icFb.click { toastMessageInComing() }
+        icIg.click { toastMessageInComing() }
+        icTelegrams.click { toastMessageInComing() }
+        buttonLogin.click { eventLogin() }
+    }
+
+    override fun fetchDataSource() = viewModel.apply {
+        if (isNetworkAvailable(this@LoginActivity)) fetchDataUser() else showNetworkError()
+    }
+
+    override fun observerDataSource() = viewModel.apply {
+        isLoadingFetchUsers.observe(this@LoginActivity) { isLoading ->
+            if (isLoading) dialogLoading.show() else dialogLoading.cancel()
+        }
+
+        users.observe(this@LoginActivity) { listUserDto ->
+            submitListData(listUserDto)
         }
     }
+
+    private fun toastMessageInComing() = viewModel.toastMessageInComing(this)
+
+    private fun eventLogin() {
+        val email = getEmail()
+        val password = getPassword()
+        val isInputEmpty = isInputEmpty(email, password)
+
+        /* *
+           validate null input
+        * */
+        if (!isNetworkAvailable(this)) {
+            showNetworkError()
+            return
+        }; if (isInputEmpty) {
+            showEmptyInputError()
+            return
+        }
+
+        /* *
+           handle search account
+        * */
+        handleSearchUser(email = email, password = password)
+    }
+
+    private fun cacheUserToSharedPref(user: UserModelDTO) {
+        val jsonUserCache = convertObjectToJson(user)
+        SharedUtils.jsonUserCache = jsonUserCache
+    }
+
+    private fun getEmail() = binding.inputEmailAddress.text.toString().trim()
+
+    private fun getPassword() = binding.inputPassword.text.toString().trim()
+
+    private fun isInputEmpty(email: String, password: String) =
+        email.isEmpty() || password.isEmpty()
+
+    private fun showNetworkError() = toastMessageRes(R.string.no_network_connection)
+
+    private fun showEmptyInputError() = toastMessageRes(R.string.this_field_cannot_be_left_blank)
+
+    private fun showAccountNotFound() = toastMessageRes(R.string.account_not_found)
+
+    private fun handleSearchUser(email: String, password: String) =
+        searchUser(email, password) { user ->
+            if (user != null) onUserFound(user) else onUserNotFound()
+        }
+
+    private fun searchUser(
+        email: String,
+        password: String,
+        callBack: (user: UserModelDTO?) -> Unit
+    ) =
+        viewModel.apply {
+            searchEmailAndPassword(email = email, password = password, list = listUsers)
+            callBack.invoke(userDtoAfterSearch.value)
+        }
+
+    private fun onUserFound(user: UserModelDTO) {
+        cacheUserToSharedPref(user)
+        startHomeActivity(this)
+        finish()
+    }
+
+    private fun onUserNotFound() = showAccountNotFound()
+
+    private fun submitListData(list: MutableList<UserModelDTO>) =
+        listUsers.apply {
+            clear()
+            addAll(list)
+        }
 }
--- app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginViewModel.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/login/LoginViewModel.kt
@@ -1,9 +1,80 @@
 package com.social.media.blog.ltd.ui.screen.login
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.social.media.blog.ltd.R
+import com.social.media.blog.ltd.commons.AppConst.userRef
+import com.social.media.blog.ltd.commons.extention.toastMessageRes
+import com.social.media.blog.ltd.model.dto.UserModelDTO
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
 
-    
+    private val _users = MutableLiveData<MutableList<UserModelDTO>>()
+    private val _isLoadingFetchUsers = MutableLiveData<Boolean>()
+    private val _userDtoAfterSearch = MutableLiveData<UserModelDTO?>()
 
+    val users: LiveData<MutableList<UserModelDTO>> = _users
+    val isLoadingFetchUsers: LiveData<Boolean> = _isLoadingFetchUsers
+    val userDtoAfterSearch: LiveData<UserModelDTO?> = _userDtoAfterSearch
+
+    fun toastMessageInComing(context: Context) =
+        context.toastMessageRes(R.string.this_feature_will_be_released_soon)
+
+    fun fetchDataUser() = viewModelScope.launch(Dispatchers.IO) {
+        _isLoadingFetchUsers.postValue(true)
+        val resultListUser = fetchDataUserReturnResult()
+        _users.postValue(resultListUser)
+        _isLoadingFetchUsers.postValue(false)
+    }
+
+    fun searchEmailAndPassword(email: String, password: String, list: MutableList<UserModelDTO>) =
+        viewModelScope.launch(Dispatchers.IO) {
+            _isLoadingFetchUsers.postValue(true)
+            val resultAfterSearch = searchEmailAndPasswordReturnResult(email, password, list)
+            _userDtoAfterSearch.postValue(resultAfterSearch)
+            _isLoadingFetchUsers.postValue(false)
+        }
+
+
+    private suspend fun searchEmailAndPasswordReturnResult(
+        email: String,
+        password: String,
+        list: MutableList<UserModelDTO>
+    ): UserModelDTO? = suspendCoroutine { continuation ->
+        continuation.resume(list.firstOrNull { user ->
+            user.email == email && user.password == password
+        })
+    }
+
+    private suspend fun fetchDataUserReturnResult(): MutableList<UserModelDTO> =
+        suspendCoroutine { continuation ->
+            val listener = object : ValueEventListener {
+                override fun onDataChange(dataSnapShot: DataSnapshot) {
+                    val listUsersModelDto = mutableListOf<UserModelDTO>()
+                    for (userSnapShot in dataSnapShot.children) {
+                        val userModelDto = userSnapShot.getValue(UserModelDTO::class.java)
+                        if (userModelDto != null) {
+                            listUsersModelDto.add(userModelDto)
+                        }
+                    }
+
+                    continuation.resume(listUsersModelDto)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    continuation.resume(mutableListOf())
+                }
+            }
+            userRef.addValueEventListener(listener)
+        }
 }
