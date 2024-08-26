043ada1563630603f352cd6a0efcc87a1b406f8e
duythanh7903 <beedyto@gmail.com>
2024-08-26 22:35:17 +0700

    init home screen

---
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/home/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.social.media.blog.ltd.ui.screen.home
+
+import com.social.media.blog.ltd.databinding.ActivityHomeBinding
+import com.social.media.blog.ltd.ui.base.BaseActivity
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+
+    override fun inflateViewBinding(): ActivityHomeBinding =
+        ActivityHomeBinding.inflate(layoutInflater)
+
+}
