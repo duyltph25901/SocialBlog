f29c1a0b81b7ca1647a32b40b393946b99a2e6c9
duythanh7903 <beedyto@gmail.com>
2024-09-04 00:05:08 +0700

    add fun ext

--- app/src/main/java/com/social/media/blog/ltd/commons/extention/ContextExt.kt
+++ app/src/main/java/com/social/media/blog/ltd/commons/extention/ContextExt.kt
@@ -1,9 +1,13 @@
 package com.social.media.blog.ltd.commons.extention
 
 import android.content.Context
+import android.os.Build
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.social.media.blog.ltd.commons.ConvertUtils.convertJsonToObjectConvertUtils
 import com.social.media.blog.ltd.commons.ConvertUtils.convertObjectToJsonConvertUtils
+import com.social.media.blog.ltd.commons.PermissionUtils
 import java.util.Locale
 import java.util.regex.Pattern
 
@@ -42,5 +46,22 @@ fun Context.validatePassword(password: String): Boolean = password.length >= 6
 
 fun <T> Context.convertObjectToJson(obj: T): String = convertObjectToJsonConvertUtils(obj = obj)
 
-fun <T> Context.convertJsonToObject(json: String, classOfT: Class<T>): T? =
+fun <T> Context.convertJsonToObject(json: String, classOfT: Class<T>): T =
     convertJsonToObjectConvertUtils(json = json, classOfT = classOfT)
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+fun Context.isReadMediaImageAccepted() = PermissionUtils.isReadMediaImageAccepted(this)
+
+fun Context.isReadExternalStorageAccepted() = PermissionUtils.isReadExternalStorageAccepted(this)
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+fun Context.requestReadMediaImagePermission(fragment: Fragment, callback: () -> Unit) =
+    PermissionUtils.requestReadMediaImagePermission(fragment, callback)
+
+fun Context.requestReadExternalStoragePermission(fragment: Fragment, callback: () -> Unit) =
+    PermissionUtils.requestReadExternalPermission(fragment, callback)
+
+fun Context.isCameraPermissionAccepted() = PermissionUtils.isCameraPermissionAccepted(this)
+
+fun Context.requestCameraPermission(fragment: Fragment, callback: () -> Unit) =
+    PermissionUtils.requestCameraPermission(fragment, callback)
