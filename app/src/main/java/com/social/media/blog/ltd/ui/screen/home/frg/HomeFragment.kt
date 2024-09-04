16f6cbd94d23239895ddda0282c2f38d88fe0d58
duythanh7903 <beedyto@gmail.com>
2024-09-04 22:16:35 +0700

    create fun like, comment, share, show detail post

--- app/src/main/java/com/social/media/blog/ltd/ui/screen/home/frg/HomeFragment.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/home/frg/HomeFragment.kt
@@ -1,11 +1,16 @@
 package com.social.media.blog.ltd.ui.screen.home.frg
 
+import android.os.Bundle
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.lifecycleScope
 import com.social.media.blog.ltd.R
+import com.social.media.blog.ltd.commons.AppConst.KEY_SHOW_POST_DETAIL
+import com.social.media.blog.ltd.commons.Routes
 import com.social.media.blog.ltd.commons.SharedUtils
 import com.social.media.blog.ltd.commons.extention.convertObjectToJson
 import com.social.media.blog.ltd.commons.extention.goneView
+import com.social.media.blog.ltd.commons.extention.hideKeyboard
+import com.social.media.blog.ltd.commons.extention.toastMessageRes
 import com.social.media.blog.ltd.commons.extention.visibleView
 import com.social.media.blog.ltd.databinding.FragmentHomeBinding
 import com.social.media.blog.ltd.model.dto.CategoryModelDTO
@@ -13,7 +18,13 @@ import com.social.media.blog.ltd.model.dto.PostModelDTO
 import com.social.media.blog.ltd.ui.adapter.rcv.CategoryAdapter
 import com.social.media.blog.ltd.ui.adapter.rcv.PostAdapterTypeOne
 import com.social.media.blog.ltd.ui.base.BaseFragment
+import com.social.media.blog.ltd.ui.dialog.LoadingResponseDialog
 import com.social.media.blog.ltd.ui.screen.home.frg.vm.HomeViewModel
+import com.social.media.blog.ltd.ui.screen.home.frg.vm.HomeViewModel.Companion.FLAG_REQUEST_API_TRUE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
@@ -21,12 +32,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     private lateinit var categoryAdapter: CategoryAdapter
     private lateinit var postAdapterOne: PostAdapterTypeOne
+    private lateinit var loadingDialog: LoadingResponseDialog
 
     override fun getLayoutFragment(): Int = R.layout.fragment_home
 
     override fun initVariables() {
         initCategoryAdapter()
         initPostAdapterOne()
+        initDialogResponseLoading()
     }
 
     override fun initViews() {
@@ -57,7 +70,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun observerData() {
         viewModel.apply {
             isLoadingCategory.observe(viewLifecycleOwner) { loading ->
-                if (loading) mBinding.loadingCategory.visibleView() else mBinding.loadingCategory.goneView()
+                if (loading) showLoadingCategory() else hideLoadingCategory()
             }
 
             categories.observe(viewLifecycleOwner) { listCategories ->
@@ -66,13 +79,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
             }
 
             isLoadingPostOne.observe(viewLifecycleOwner) { isLoading ->
-                if (isLoading) mBinding.loadingPost1.visibleView()
-                else mBinding.loadingPost1.goneView()
+                if (isLoading) showLoadingPostOne() else hideLoadingPostOne()
+            }
+
+            isLoadingResponse.observe(viewLifecycleOwner) { isLoading ->
+                if (isLoading) showDialogResponse() else hideDialogResponse()
             }
 
             posts.observe(viewLifecycleOwner) { listPosts ->
                 setNewDataPost(listPosts)
             }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        viewModel.apply {
+            removeEventListenerCategories()
+            removeEventListenerPosts()
         }
     }
 
@@ -90,28 +115,147 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     private fun setNewDataCategory(newData: MutableList<CategoryModelDTO>) =
         categoryAdapter.submitData(newData)
-    
+
     private fun cacheCategoryPostIfCan(objectCategoryDefault: CategoryModelDTO, indexCache: Int) {
         if (isCategoryDefaultNull()) {
             val jsonCategory = convertCategoryToJson(objectCategoryDefault)
             handleCacheCategoryDefault(jsonCategory, indexCache)
-        } 
+        }
     }
-    
+
     private fun convertCategoryToJson(category: CategoryModelDTO) =
-        mBinding.root.context.convertObjectToJson(category)
-    
+        getContextScreen().convertObjectToJson(category)
+
     private fun isCategoryDefaultNull() = SharedUtils.jsonCategory == "{}"
-    
+
     private fun handleCacheCategoryDefault(jsonCategory: String, indexCache: Int) {
         SharedUtils.jsonCategory = jsonCategory
         SharedUtils.indexCategory = indexCache
     }
 
     private fun initPostAdapterOne() {
-        postAdapterOne = PostAdapterTypeOne()
+        postAdapterOne = PostAdapterTypeOne(likedPost = { post, index ->
+            likePost(post, index)
+        }, commentPost = { post, index, comment ->
+            commentPost(post, index, comment)
+        }, sharePost = { _, _ ->
+            toastMessageInComing()
+        }, showDetailsPost = { post, index ->
+            showDetailPost(post, index)
+        })
     }
+
+    private fun initDialogResponseLoading() {
+        loadingDialog = LoadingResponseDialog(getContextScreen(), false)
+    }
+
+    private fun getContextScreen() = mBinding.root.context
 
     private fun setNewDataPost(newData: MutableList<PostModelDTO>) =
         postAdapterOne.submitData(newData)
+
+    private fun showLoadingCategory() =
+        mBinding.loadingCategory.visibleView()
+
+    private fun hideLoadingCategory() =
+        mBinding.loadingCategory.goneView()
+
+    private fun showLoadingPostOne() =
+        mBinding.loadingPost1.visibleView()
+
+    private fun hideLoadingPostOne() =
+        mBinding.loadingPost1.goneView()
+
+    private fun showDialogResponse() = loadingDialog.show()
+
+    private fun hideDialogResponse() = loadingDialog.cancel()
+
+    private fun likePost(post: PostModelDTO, index: Int) =
+        lifecycleScope.launch(Dispatchers.IO) {
+            val defUpdate = viewModel.handleLikePost(post, getContextScreen())
+            val flagAfterUpdate = defUpdate.await()
+
+            defUpdate.cancel()
+            when (flagAfterUpdate) {
+                FLAG_REQUEST_API_TRUE ->
+                    withContext(Dispatchers.Main) { refreshItemAtPositionRcv(index) }
+
+                else -> withContext(Dispatchers.Main) { showMessageSomeThingWentWrong() }
+            }
+            // cancel coroutine
+            cancel()
+        }
+
+    private fun showMessageSomeThingWentWrong() =
+        getContextScreen().toastMessageRes(R.string.some_thing_went_wrong)
+
+    private fun refreshItemAtPositionRcv(index: Int) =
+        postAdapterOne.notifyItemChanged(index)
+
+    private fun commentPost(post: PostModelDTO, index: Int, comment: String) {
+        if (isCommentNull(comment)) {
+            showMessageNullInput()
+            return
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val defUpdate = viewModel.handleAddComment(post, comment, getContextScreen())
+            val flagAfterUpdate = defUpdate.await()
+
+            defUpdate.cancel()
+            when (flagAfterUpdate) {
+                FLAG_REQUEST_API_TRUE ->
+                    withContext(Dispatchers.Main) {
+                        hideInput()
+                        refreshItemAtPositionRcv(index)
+                    }
+
+                else -> withContext(Dispatchers.Main) { showMessageSomeThingWentWrong() }
+            }
+
+            // cancel coroutine
+            cancel()
+        }
+    }
+
+    private fun isCommentNull(comment: String) = comment.isEmpty()
+
+    private fun showMessageNullInput() =
+        getContextScreen().toastMessageRes(R.string.this_field_cannot_be_left_blank)
+
+    private fun hideInput() =
+        getContextScreen().hideKeyboard(mBinding.root)
+
+    private fun toastMessageInComing() =
+        getContextScreen().toastMessageRes(R.string.this_feature_will_be_released_soon)
+
+    private fun showDetailPost(post: PostModelDTO, index: Int) =
+        lifecycleScope.launch(Dispatchers.IO) {
+            val defUpdate = viewModel.updateViewPostPlusOne(post)
+            val flagAfterUpdate = defUpdate.await()
+
+            defUpdate.cancel()
+            when (flagAfterUpdate) {
+                FLAG_REQUEST_API_TRUE ->
+                    withContext(Dispatchers.Main) {
+                        handleShowDetailPost(post)
+                        refreshItemAtPositionRcv(index)
+                    }
+
+                else -> withContext(Dispatchers.Main) { showMessageSomeThingWentWrong() }
+            }
+        }
+
+    private fun handleShowDetailPost(post: PostModelDTO) {
+        val bundle = getBundlePostDetail(post)
+        startPostDetailActivity(bundle)
+    }
+
+    private fun getBundlePostDetail(post: PostModelDTO) =
+        Bundle().apply {
+            putSerializable(KEY_SHOW_POST_DETAIL, post)
+        }
+
+    private fun startPostDetailActivity(bundle: Bundle) =
+        Routes.startPostDetailActivity(requireActivity(), bundle)
 }
--- app/src/main/java/com/social/media/blog/ltd/ui/screen/home/frg/vm/HomeViewModel.kt
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/home/frg/vm/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.social.media.blog.ltd.ui.screen.home.frg.vm
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,20 +10,33 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.social.media.blog.ltd.commons.AppConst.categoryRef
 import com.social.media.blog.ltd.commons.AppConst.postRef
+import com.social.media.blog.ltd.commons.SharedUtils
+import com.social.media.blog.ltd.commons.extention.convertJsonToObject
 import com.social.media.blog.ltd.model.dto.CategoryModelDTO
 import com.social.media.blog.ltd.model.dto.PostModelDTO
+import com.social.media.blog.ltd.model.dto.UserModelDTO
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
+
+    companion object {
+        internal const val FLAG_REQUEST_API_TRUE = 1
+        internal const val FLAG_REQUEST_API_FAIL = 0
+    }
 
     private val _isLoadingCategory = MutableLiveData<Boolean>()
     private val _isLoadingPostOne = MutableLiveData<Boolean>()
+    private val _isLoadingResponse = MutableLiveData<Boolean>()
     private val _categories = MutableLiveData<MutableList<CategoryModelDTO>>()
     private val _posts = MutableLiveData<MutableList<PostModelDTO>>()
 
     val isLoadingCategory: LiveData<Boolean> = _isLoadingCategory
     val isLoadingPostOne: LiveData<Boolean> = _isLoadingPostOne
+    val isLoadingResponse: LiveData<Boolean> = _isLoadingResponse
     val categories: LiveData<MutableList<CategoryModelDTO>> = _categories
     val posts: LiveData<MutableList<PostModelDTO>> = _posts
 
@@ -44,7 +58,7 @@ class HomeViewModel: ViewModel() {
 
     fun removeEventListenerPosts() = postRef.removeEventListener(onEventListenerPost)
 
-    private fun createCategoriesEventListener() = object: ValueEventListener {
+    private fun createCategoriesEventListener() = object : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
             val listCategories = getListCategoriesFromSnapShot(snapshot)
             _categories.postValue(listCategories)
@@ -60,14 +74,14 @@ class HomeViewModel: ViewModel() {
         val listCategories = mutableListOf<CategoryModelDTO>()
         for (categorySnapShot in dataSnapshot.children) {
             val categoryModelDTO = categorySnapShot.getValue(CategoryModelDTO::class.java)
-            if (categoryModelDTO!= null) {
+            if (categoryModelDTO != null) {
                 listCategories.add(categoryModelDTO)
             }
         }
         return listCategories
     }
 
-    private fun createPostEventListener() = object: ValueEventListener {
+    private fun createPostEventListener() = object : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
             val listPosts = getListPostsFromSnapShot(snapshot)
             _posts.postValue(listPosts)
@@ -82,7 +96,7 @@ class HomeViewModel: ViewModel() {
         val listPosts = mutableListOf<PostModelDTO>()
         for (categorySnapShot in snapshot.children) {
             val postModelDTO = categorySnapShot.getValue(PostModelDTO::class.java)
-            if (postModelDTO!= null) {
+            if (postModelDTO != null) {
                 listPosts.add(postModelDTO)
             }
         }
@@ -95,4 +109,76 @@ class HomeViewModel: ViewModel() {
         postRef.addValueEventListener(onEventListenerPost)
         _isLoadingPostOne.postValue(false)
     }
+
+    fun handleLikePost(post: PostModelDTO, context: Context) =
+        viewModelScope.async(Dispatchers.IO) {
+            postValueShowDialogLoading()
+            val jsonCurrentUser = getJsonUserCache()
+            val currentUser = convertJsonToObject(jsonCurrentUser, UserModelDTO::class.java, context)
+            val idCurrentUser = getIdUser(currentUser)
+            post.listIdUserLiked = updateListUserLiked(post, idCurrentUser)
+            // update real time
+            val responseAfterUpdate = updatePostAndReturnFlagUpdate(post)
+            postValueHideDialogLoading()
+            return@async responseAfterUpdate
+        }
+
+    private fun isUserLikedPostBefore(post: PostModelDTO, idUser: String) =
+        post.listIdUserLiked.contains(idUser)
+
+    private suspend fun updatePostAndReturnFlagUpdate(post: PostModelDTO) =
+        suspendCoroutine { continuation ->
+            postRef.child(post.id).setValue(post)
+                .addOnSuccessListener { continuation.resume(FLAG_REQUEST_API_TRUE) }
+                .addOnFailureListener { continuation.resume(FLAG_REQUEST_API_FAIL) }
+        }
+
+    private fun <T> convertJsonToObject(json: String, classOfT: Class<T>, context: Context): T =
+        context.convertJsonToObject(json, classOfT)
+
+    private fun getIdUser(user: UserModelDTO) = user.idUser
+
+    private fun getJsonUserCache() = SharedUtils.jsonUserCache
+
+    private fun postValueShowDialogLoading() = _isLoadingResponse.postValue(true)
+
+    private fun postValueHideDialogLoading() = _isLoadingResponse.postValue(false)
+
+    private fun updateListUserLiked(post: PostModelDTO, idUser: String) =
+        post.listIdUserLiked.apply {
+            if (isUserLikedPostBefore(post, idUser)) remove(idUser)
+            else add(idUser)
+        }
+
+    fun handleAddComment(post: PostModelDTO, comment: String, context: Context) =
+        viewModelScope.async(Dispatchers.IO) {
+            postValueShowDialogLoading()
+            val jsonCurrentUser = getJsonUserCache()
+            val currentUser = convertJsonToObject(jsonCurrentUser, UserModelDTO::class.java, context)
+            val idCurrentUser = getIdUser(currentUser)
+            post.listComments = updateListUserComment(post, comment, idCurrentUser)
+            // update real time
+            val responseAfterUpdate = updatePostAndReturnFlagUpdate(post)
+            postValueHideDialogLoading()
+            return@async responseAfterUpdate
+        }
+
+    private fun updateListUserComment(post: PostModelDTO, comment: String, idUser: String) =
+        post.listComments.apply {
+            add(
+                PostModelDTO.Comment(
+                idUser = idUser,
+                comment = comment,
+            ))
+        }
+
+    fun updateViewPostPlusOne(post: PostModelDTO) =
+        viewModelScope.async(Dispatchers.IO) {
+            postValueShowDialogLoading()
+            post.views += 1
+            // update real time
+            val responseAfterUpdate = updatePostAndReturnFlagUpdate(post)
+            postValueHideDialogLoading()
+            return@async responseAfterUpdate
+        }
 }
