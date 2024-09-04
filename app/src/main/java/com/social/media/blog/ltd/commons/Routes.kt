22cf7a1d821033cbbfaa7f3427510915926280a9
duythanh7903 <beedyto@gmail.com>
2024-09-04 22:13:59 +0700

    create fun navigation to post detail screen

--- app/src/main/java/com/social/media/blog/ltd/commons/Routes.kt
+++ app/src/main/java/com/social/media/blog/ltd/commons/Routes.kt
@@ -2,9 +2,11 @@ package com.social.media.blog.ltd.commons
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
 import com.social.media.blog.ltd.ui.screen.home.HomeActivity
 import com.social.media.blog.ltd.ui.screen.intro.IntroActivity
 import com.social.media.blog.ltd.ui.screen.login.LoginActivity
+import com.social.media.blog.ltd.ui.screen.post.PostDetailActivity
 import com.social.media.blog.ltd.ui.screen.register.RegisterActivity
 
 object Routes {
@@ -26,6 +28,12 @@ object Routes {
 
     fun startHomeActivity(fromActivity: Activity) =
         Intent(fromActivity, HomeActivity::class.java).apply {
+            fromActivity.startActivity(this)
+        }
+
+    fun startPostDetailActivity(fromActivity: Activity, bundle: Bundle) =
+        Intent(fromActivity, PostDetailActivity::class.java).apply {
+            putExtras(bundle)
             fromActivity.startActivity(this)
         }
 
