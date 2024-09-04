edd317dfbff5c84b9cbaf478c213f42b27266667
duythanh7903 <beedyto@gmail.com>
2024-09-04 22:15:02 +0700

    create post details screen

---
+++ app/src/main/java/com/social/media/blog/ltd/ui/screen/post/PostDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.social.media.blog.ltd.ui.screen.post
+
+import com.social.media.blog.ltd.databinding.ActivityPostDetailBinding
+import com.social.media.blog.ltd.ui.base.BaseActivity
+
+class PostDetailActivity : BaseActivity<ActivityPostDetailBinding>() {
+
+    override fun inflateViewBinding(): ActivityPostDetailBinding = ActivityPostDetailBinding.inflate(layoutInflater)
+
+
+}
